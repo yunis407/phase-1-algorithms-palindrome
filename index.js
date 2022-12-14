23fa2428a89ad91eba5213fb89cf5e87edbdc2a3
yunisaliosman <yunis.osman@student.moringaschool.com>
2022-12-14 11:10:08 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 function isPalindrome(word) {
   // Write your algorithm here
+
+  let newWord =word.split("").reverse().join("");
+  if (word == newWord) {
+    return true;
+  } else if (word != newWord) {
+    return false;
+  }
 }
 
 /* 
   Add your pseudocode here
+  
+  declare a newWord variable,split it to substrings,reverse them and join them to form a string.
+  if the word is the same as the newWord return true else return false
 */
 
 /*
   Add written explanation of your solution here
+  The word is split into substrings ,the substrings are reversed and then joined to form a complete string.
+  if the word is the same as the newWord its a palindrome hence returns true else if it's not a palindrome it returns false.
 */
 
 // You can run `node index.js` to view these console logs
